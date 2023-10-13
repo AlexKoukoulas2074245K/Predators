e5c41f3044da6279ddb5d5d2da074ff584a67b4f
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2023-10-13 19:46:13 +0100

    Hid home indicator

--- source_ios/platform_specific/CoreSystemsEnginePlatformImpl.cpp
+++ source_ios/platform_specific/CoreSystemsEnginePlatformImpl.cpp
@@ -69,6 +69,8 @@ void CoreSystemsEngine::Initialize()
 
     // Create window
     SDL_SetHint(SDL_HINT_ORIENTATIONS, "LandscapeLeft LandscapeRight");
+    SDL_SetHint(SDL_HINT_IOS_HIDE_HOME_INDICATOR, "2");
+    
     mWindow = SDL_CreateWindow("Predators", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
     // Set minimum window size
