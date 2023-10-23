8c3a7d30254ba8e233bfeb136f6f3d8b893d9937
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2023-10-23 12:28:35 +0100

    Updated card frame textures

--- source_desktop/platform_specific/RendererPlatformImpl.cpp
+++ source_desktop/platform_specific/RendererPlatformImpl.cpp
@@ -354,7 +354,7 @@ static SceneObjectDataIMGuiVisitor imguiVisitor;
 
 void RendererPlatformImpl::CreateIMGuiWidgets()
 {
-    ImGui::ShowDemoWindow();
+    //ImGui::ShowDemoWindow();
     
     auto& resService = CoreSystemsEngine::GetInstance().GetResourceLoadingService();
     
