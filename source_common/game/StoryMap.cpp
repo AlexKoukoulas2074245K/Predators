cb4bc09a65c88878af8ed7e261b93660f42002f9
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2024-02-21 13:54:12 +0000

    Tweaked elite and boss stat factors

--- source_common/game/StoryMap.cpp
+++ source_common/game/StoryMap.cpp
@@ -118,8 +118,8 @@ static const float INACTIVE_NODE_PATH_LIFETIME_SECS = 0.85f;
 static const float SELECTABLE_NODE_BOUNCE_SPEED_Y = 0.000005f;
 static const float PORTRAIT_BOUNCE_NOISE_FACTOR = 0.2f;
 static const float INACTIVE_NODE_TEXT_ALPHA = 0.5f;
-static const float ELITE_STAT_FACTOR = 1.25f;
-static const float BOSS_STAT_FACTOR = 1.4f;
+static const float ELITE_STAT_FACTOR = 1.15f;
+static const float BOSS_STAT_FACTOR = 1.25f;
 static const float TUTORIAL_MAP_DOWNSCALE_FACTOR = 1.0f/3.0f;
 
 static const int MAP_PATH_SEGMENTS_FACTOR = 30;
